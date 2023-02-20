280e8b1da7c64911c8f53094b9ae09b0fc757c98
Alex Burgess <aeb.qaz@gmail.com>
2023-02-20 21:06:28 +1300

    Initial plugin config

--- src/Plugin.php
+++ src/Plugin.php
@@ -8,4 +8,24 @@ use Cake\Core\BasePlugin;
  * Plugin for WebpackAssetLoader
  */
 class Plugin extends BasePlugin {
+	/**
+     * Plugin name.
+     *
+     * @var string
+     */
+    protected $name = 'WebpackAssetLoader';
+
+    /**
+     * Do bootstrapping or not
+     *
+     * @var bool
+     */
+    protected $bootstrapEnabled = false;
+
+    /**
+     * Load routes or not
+     *
+     * @var bool
+     */
+    protected $routesEnabled = false;
 }
